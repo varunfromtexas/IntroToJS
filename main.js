3898c1a0706cf0b588b5541b55753451f86c2975
Varun Ambrose <varunambrose@Varuns-MacBook-Pro.local>
2017-07-25 12:27:41 -0500

    Intro to JS on 7.25

---
+++ main.js
@@ -0,0 +1,11 @@
+const computer = "paper";
+let me = "scissors";
+
+if (computer == me) {
+  console.log("Looks like a tie!")
+}
+  else if (me == "rock") {
+    console.log("Computer Wins")
+  } else {
+    console.log("I win!")
+  }
